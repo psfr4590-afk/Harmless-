0e360446e4192dbb41b7aacbf6c5085feff80619
John Petty <psfr4590@gmail.com>
2026-09-22 05:46:48 -0500

    fix: correct audit findings in src/utils/medicalApi.mjs

--- src/utils/medicalApi.mjs
+++ src/utils/medicalApi.mjs
@@ -40,7 +40,7 @@ function normalizeEvidenceText(value) {
 }
 
 function escapeFdaSearchTerm(term) {
-  return String(term).replace(/[undefined"()]/g, '\\$&');
+  return String(term).replace(/[\\"]/g, '\\$&');
 }
 function containsTerm(text, term) {
   const haystack = normalizeEvidenceText(text);
